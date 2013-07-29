40e18c464cb5d1b07c11a3c7f3d53adb7ead157b
Alex Loranger <alex.loranger.1988@gmail.com>
2013-07-29 10:31:49 -0700

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# -==- Operator Names -==-
+############################################################################################################################
+
+# [  +  ]    plus			addition
+# [  -  ] 	minus			subtraction
+# [  /  ] 	slash			division
+# [  *  ] 	asterisk		multiplication
+# [  %  ] 	percent		 	divides then uses remainder 	
+# [  <  ] 	less-than		tests whether less-than, outputting True/False
+# [  >  ] 	greater-than		tests whether greater-than, outputting True/False
+# [  <= ] 	less-than-equal		tests whether less-than/equal, outputting True/False
+# [  >= ]	greater-than-equal	tests whether greater-than/equal, outputting True/False
+
+# Remember PEMDAS !!!
+
+############################################################################################################################
+
+# prints text to the stdout explaining what is about to be performed
+print "I will now count my chickens:"
+
+# prints "Hens" and performs 30 / 6 = 5, 5 + 25 = 30
+print "Hens", 25 + 30 / 6
+
+# prints "Roosters" and performs 25 * 3 = 75, 75 % 4 = 18 R3, 100 - 3 = 97
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+
+
