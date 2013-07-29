cbd4b00d85e553e2a40c6f21c07df4546dabc69e
Alex Loranger <alex.loranger.1988@gmail.com>
2013-07-29 10:31:13 -0700

    Create ex1-2.py

---
+++ ex1-2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+#print "Another line!!!"
+
+
+# A comment, this is so you can read your program later.
+# Anything after the [ # ] is ignored by python.
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to `disable' or comment out a piece of code:
+# print "This won't run."
+print "This will run."
+
+
