a8387cc46a628cab973204518d1e16779d311947
Alex Loranger <alex.loranger.1988@gmail.com>
2013-07-31 10:40:13 -0700

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# File: ex9.py
+# Description: Printing, Printing, Printing
+# Python 2.7.5 (default, May 12 2013, 12:27:12) 
+#
+
+# Here's some strange stuff, remember type it exactly.
+
+# assigns the string to the variable [ days ]
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# assigns the string to the variable [ months ]
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# prints the string, then prints the variable [ days ] on the same line
+print "Here are the days: ", days
+# prints the string, then prints the variable [ months ] on the same line
+print "Here are the months: ", months
+
+# prints the string with text formatting inbetween the [ """ ]
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines  if we want, or 5, or 6.
+"""
+
